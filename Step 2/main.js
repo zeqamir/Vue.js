9cea19fe2c520287b139fcef87a49040ba391d1d
Amir Zeqiri <amir.zeqiri@eduvaud.ch>
2024-03-18 09:35:01 +0100

    Step 2 end

---
+++ Step 2/main.js
@@ -0,0 +1,11 @@
+const product = "Socks";
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      // Solution
+      description: "Ceci est une description.",
+      // Solution
+    };
+  },
+});
